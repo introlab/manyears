ba62d9985704c63a1dab91d4ac22e255e75adb9c
jdascenzio <jdascenzio@12089078-0fe9-4cd0-90fa-723a746d8aa5>
2007-09-27 20:13:57 +0000

    Node of rtaudio

---
+++ manyears/src/info.cc
@@ -0,0 +1,120 @@
+#include "BufferedNode.h"
+#include "operators.h"
+#include "RtAudio.h"
+
+
+
+using namespace std;
+
+namespace FD {
+    
+    class info;
+    
+    DECLARE_NODE(info)
+    /*Node
+    *
+    * @name info
+    * @category ManyEars
+    * @description Print the device available
+    *
+    * @output_name OUTPUT
+    * @output_description always true
+    * @output_type bool
+    *
+    END*/
+    
+    
+    class info : public BufferedNode 
+    {
+        //Input ID            
+        //int m_inputID;
+        //Output ID
+        int m_outputID;
+        
+        public:
+        info(string nodeName, ParameterSet params)
+        : BufferedNode(nodeName, params)
+        {
+            m_outputID = addOutput("OUTPUT");
+        }      
+        
+        void calculate(int output_id, int count, Buffer &out)
+        {
+            //Handle            
+            std::map<int, std::string> apiMap;
+            apiMap[RtAudio::MACOSX_CORE] = "OS-X Core Audio";
+            apiMap[RtAudio::WINDOWS_ASIO] = "Windows ASIO";
+            apiMap[RtAudio::WINDOWS_DS] = "Windows Direct Sound";
+            apiMap[RtAudio::UNIX_JACK] = "Jack Client";
+            apiMap[RtAudio::LINUX_ALSA] = "Linux ALSA";
+            apiMap[RtAudio::LINUX_OSS] = "Linux OSS";
+            apiMap[RtAudio::RTAUDIO_DUMMY] = "RtAudio Dummy";
+            
+            std::vector< RtAudio::Api > apis;
+            RtAudio :: getCompiledApi( apis );
+            
+            std::cout << "\nCompiled APIs:\n";
+            for ( unsigned int i=0; i<apis.size(); i++ )
+                std::cout << "  " << apiMap[ apis[i] ] << std::endl;
+            
+            RtAudio audio;
+            RtAudio::DeviceInfo info;
+            
+            std::cout << "\nCurrent API: " << apiMap[ audio.getCurrentApi() ] << std::endl;
+            
+            unsigned int devices = audio.getDeviceCount();
+            std::cout << "\nFound " << devices << " device(s) ...\n";
+            
+            for (unsigned int i=0; i<devices; i++) {
+                cout<<endl<<"####### device number: "<<i<<endl;
+                info = audio.getDeviceInfo(i);
+                
+                std::cout << "\nDevice Name = " << info.name << '\n';
+                if ( info.probed == false )
+                    std::cout << "Probe Status = UNsuccessful\n";
+                else {
+                    std::cout << "Probe Status = Successful\n";
+                    std::cout << "Output Channels = " << info.outputChannels << '\n';
+                    std::cout << "Input Channels = " << info.inputChannels << '\n';
+                    std::cout << "Duplex Channels = " << info.duplexChannels << '\n';
+                    if ( info.isDefaultOutput ) std::cout << "This is the default output device.\n";
+                    else std::cout << "This is NOT the default output device.\n";
+                    if ( info.isDefaultInput ) std::cout << "This is the default input device.\n";
+                    else std::cout << "This is NOT the default input device.\n";
+                    if ( info.nativeFormats == 0 )
+                        std::cout << "No natively supported data formats(?)!";
+                    else {
+                        std::cout << "Natively supported data formats:\n";
+                        if ( info.nativeFormats & RTAUDIO_SINT8 )
+                            std::cout << "  8-bit int\n";
+                        if ( info.nativeFormats & RTAUDIO_SINT16 )
+                            std::cout << "  16-bit int\n";
+                        if ( info.nativeFormats & RTAUDIO_SINT24 )
+                            std::cout << "  24-bit int\n";
+                        if ( info.nativeFormats & RTAUDIO_SINT32 )
+                            std::cout << "  32-bit int\n";
+                        if ( info.nativeFormats & RTAUDIO_FLOAT32 )
+                            std::cout << "  32-bit float\n";
+                        if ( info.nativeFormats & RTAUDIO_FLOAT64 )
+                            std::cout << "  64-bit float\n";
+                    }
+                    if ( info.sampleRates.size() < 1 )
+                        std::cout << "No supported sample rates found!";
+                    else {
+                        std::cout << "Supported sample rates = ";
+                        for (unsigned int j=0; j<info.sampleRates.size(); j++)
+                            std::cout << info.sampleRates[j] << " ";
+                    }
+                    std::cout << std::endl;
+                }
+            }
+            std::cout << std::endl;
+            
+            
+            (*(outputs[m_outputID].buffer))[count] = ObjectRef(Bool::alloc( true ));
+            
+        }
+        
+        NO_ORDER_NODE_SPEEDUP(info)
+    };
+}//namespace FD
---
+++ manyears/src/openStream.cc
@@ -0,0 +1,250 @@
+#include "BufferedNode.h"
+#include "operators.h"
+#include "RtAudio.h"
+#include "Vector.h"
+#include <QtCore/QMutex>
+#include <QtCore/QSemaphore>
+#include <QtCore/QList>
+#include "Buffer.h"
+
+using namespace std;
+
+namespace FD {
+    
+    class openStreamManyEars;
+    
+    QMutex buffersList_openStreamManyEars;
+    QSemaphore newSamplesSemaphore_openStreamManyEars;
+    
+    int inOpenStream( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
+        double streamTime, RtAudioStreamStatus status, void *data )
+    {        
+        if ( status ) std::cout << "Stream over/underflow detected." << std::endl;
+        
+        short* in = (short*) inputBuffer;
+        
+        Vector<float>* buffer = Vector<float>::alloc(nBufferFrames * 8);
+        QList<Vector<float>*>* buffersList = (QList<Vector<float>*>*) data;   
+        for(int i=0; i<nBufferFrames*8; i++)
+            (*buffer)[i]= (float)(in[i]);            
+        
+        buffersList_openStreamManyEars.lock();
+        buffersList->append( buffer );        
+        buffersList_openStreamManyEars.unlock();
+        
+        newSamplesSemaphore_openStreamManyEars.release(1);
+        
+        return 0;
+    }    
+    
+    DECLARE_NODE(openStreamManyEars)
+    
+    /*Node
+    *
+    * @name openStreamManyEars
+    * @category ManyEars
+    * @description open a stream audio
+    *
+    * @parameter_name DEVICE
+    * @parameter_type string
+    * @parameter_value M-Audio Delta
+    * @parameter_description device name
+    *
+    * @parameter_name SAMPLERATE
+    * @parameter_type int
+    * @parameter_value 48000
+    * @parameter_description sampleRate
+    *
+    * @parameter_name LENGTH
+    * @parameter_type int
+    * @parameter_value 8192
+    * @parameter_description number of samples
+    *
+    * @parameter_name ADVANCE
+    * @parameter_type int
+    * @parameter_value 4096
+    * @parameter_description number of samples
+    *
+    * @parameter_name NCHANNELS
+    * @parameter_type int
+    * @parameter_value 8
+    * @parameter_description number of channels
+    *
+    * @parameter_name FIRSTCHANNEL
+    * @parameter_type int
+    * @parameter_value 0
+    * @parameter_description first Channel
+    *
+    * @output_name OUTPUT
+    * @output_type Vector<float>
+    * @output_description the audio data
+    *
+    END*/
+    
+    
+    class openStreamManyEars : public BufferedNode 
+    {
+        
+        //Output ID
+        int m_outputID;
+        
+        //Parameters
+        int m_sampleRate;
+        int m_length;
+        int m_advance;
+        int m_nChannels;
+        int m_firstChannel;
+        string m_device;
+        
+        RtAudio m_adac;
+        
+        //map of the line_type
+        map<string,int> m_formatMap;
+        
+        //variable
+        QList<Vector<float>*> m_buffersList;
+        
+    public:
+        ~openStreamManyEars()
+        {
+            m_adac.stopStream();
+            if ( m_adac.isStreamOpen() ) m_adac.closeStream();
+        }
+        
+        openStreamManyEars(string nodeName, ParameterSet params)
+        : BufferedNode(nodeName, params)
+        {
+            m_outputID = addOutput("OUTPUT");
+            //Initialize parameters    
+            m_device = object_cast<String>(parameters.get("DEVICE"));
+            m_sampleRate = dereference_cast<int>(parameters.get("SAMPLERATE"));
+            m_length = dereference_cast<int>(parameters.get("LENGTH"));
+            m_advance = dereference_cast<int>(parameters.get("ADVANCE"));
+            m_nChannels = dereference_cast<int>(parameters.get("NCHANNELS"));
+            m_firstChannel = dereference_cast<int>(parameters.get("FIRSTCHANNEL"));
+
+
+            unsigned int devices = m_adac.getDeviceCount();
+            if ( devices < 1 )
+                throw new GeneralException("No audio devices found",__FILE__,__LINE__);
+            
+            RtAudio::DeviceInfo info;
+            unsigned int deviceID;
+            unsigned int i;
+            for (i=0; i<devices; i++) 
+            {
+                info = m_adac.getDeviceInfo(i);
+                size_t found = info.name.find(m_device.c_str());
+                if(info.name.find(m_device.c_str()) != string::npos )
+                {
+                    deviceID = i;
+                    break;                        
+                }
+            }
+            if(i==devices)
+                throw new GeneralException("device: " + m_device + "don't find" ,__FILE__,__LINE__);
+            
+            // Set the input parameters.
+            RtAudio::StreamParameters iParams;
+            
+            iParams.deviceId = deviceID;
+            iParams.nChannels = m_nChannels;
+            iParams.firstChannel = m_firstChannel;
+
+            
+            if(m_advance > m_length)
+            {
+                throw new GeneralException("Error : ADVANCE must be inferior to LENGTH",__FILE__,__LINE__);                
+            }            
+            if(m_length%m_length)
+            {
+                throw new GeneralException("Error : LENGTH % ADVANCE must be equal to zero",__FILE__,__LINE__);                
+            }
+            if(m_advance%m_nChannels)
+            {
+                throw new GeneralException("Error : ADVANCE % NCHANNELS must be equal to zero",__FILE__,__LINE__);                
+            }
+            
+            unsigned int nSamples = (int)(m_advance / m_nChannels);
+            
+            try
+            {
+                m_adac.openStream( NULL, &iParams, RTAUDIO_SINT16, m_sampleRate, &nSamples, &inOpenStream, &m_buffersList);                
+                cout<<"openStreamManyEars -> openStreamManyEars -> OK"<<endl;
+            }
+            catch ( RtError& e )
+            {
+                throw new GeneralException(e.getMessage(),__FILE__,__LINE__);
+            }
+            
+            try 
+            {
+                m_adac.startStream();
+                cout<<"openStreamManyEars -> startStream -> OK"<<endl;
+            }
+            catch ( RtError& e )
+            {
+                throw new GeneralException(e.getMessage(),__FILE__,__LINE__);
+                
+            }
+            
+            
+        }
+        
+        virtual void initialize()
+        {
+            outputs[m_outputID].lookAhead += 1;           
+            this->BufferedNode::initialize();
+        }
+        
+        void calculate(int output_id, int count, Buffer &out)
+        {
+            
+            Vector<float> *output = Vector<float>::alloc(m_length);            
+            
+            int outputSize = 0;
+            
+            if (count == 0)
+            {                                
+                
+                while( outputSize < m_length)
+                {
+                    
+                    newSamplesSemaphore_openStreamManyEars.acquire(1);
+                    
+                    buffersList_openStreamManyEars.lock();
+                    Vector<float> *buffer = m_buffersList.takeFirst();
+                    buffersList_openStreamManyEars.unlock();
+                    memcpy(&((*output)[outputSize]),&((*buffer)[0]),m_advance * sizeof(float));
+                    buffer->destroy();
+                    outputSize += m_advance;
+                }                
+                
+            }
+            else
+            {
+                Vector<float> *previous = &(object_cast<Vector<float> > ((*(outputs[m_outputID].buffer))[count-1]));
+                memcpy(&((*output)[outputSize]),&((*previous)[m_advance]),(m_length - m_advance) * sizeof(float));
+                outputSize += m_length - m_advance;                
+                while( outputSize < m_length)
+                {
+                    
+                    newSamplesSemaphore_openStreamManyEars.acquire(1);
+                    
+                    buffersList_openStreamManyEars.lock();
+                    Vector<float> *buffer = m_buffersList.takeFirst();
+                    buffersList_openStreamManyEars.unlock();
+                    
+                    memcpy(&((*output)[outputSize]),&((*buffer)[0]),m_advance * sizeof(float));        
+                    buffer->destroy();
+                    outputSize += m_advance;
+                }
+            }
+            
+            (*(outputs[m_outputID].buffer))[count] = ObjectRef(output);
+            
+        }
+        
+        NO_ORDER_NODE_SPEEDUP(openStreamManyEars)
+    };
+}//namespace FD
