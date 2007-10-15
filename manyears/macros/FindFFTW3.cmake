5b1dfcea44167f338012146325fc13e2a490c7ea
maestro <maestro@12089078-0fe9-4cd0-90fa-723a746d8aa5>
2007-10-15 18:07:58 +0000

    OSX support.

---
+++ manyears/macros/FindFFTW3.cmake
@@ -0,0 +1,11 @@
+# - Check for the presence of FFTW3## The following variables are set when FFTW3 is found:#  HAVE_FFTW3       = Set to true, if all components of FFTW3#                          have been found.#  FFTW3_INCLUDE_DIR = Include path for the header files of FFTW3#  FFTW3_LIBRARY     = Link these to use FFTW3## -----------------------------------------------------------------------------## Check for the header filesMESSAGE (STATUS "CHECKING FOR FFTW3...")
+FIND_PATH (FFTW3_INCLUDE_DIR fftw3.h  PATHS /usr/local/include /opt/local/fftw3/include /opt/local/fftw3/include/ /usr/include /sw/include)
+
+MESSAGE(STATUS "FFTW3_INCLUDE_DIR : ${FFTW3_INCLUDE_DIR}")
+## -----------------------------------------------------------------------------## Check for the library
+
+FIND_LIBRARY (FFTW3_LIBRARY fftw3f  PATHS /usr/local/lib /usr/local/fftw3/lib /usr/lib /lib /sw/lib)
+
+MESSAGE(STATUS "FFTW3_LIBRARY : ${FFTW3_LIBRARY}")
+## -----------------------------------------------------------------------------## Actions taken when all components have been foundIF (FFTW3_INCLUDE_DIR AND FFTW3_LIBRARY)  SET (HAVE_FFTW3 TRUE)ELSE (FFTW3_INCLUDE_DIR AND FFTW3_LIBRARY)  IF (NOT FFTW3_FIND_QUIETLY)    IF (NOT FFTW3_INCLUDE_DIR)      MESSAGE (STATUS "Unable to find FFTW3 header files!")    ENDIF (NOT FFTW3_INCLUDE_DIR)    IF (NOT FFTW3_LIBRARY)      MESSAGE (STATUS "Unable to find FFTW3 library files!")    ENDIF (NOT FFTW3_LIBRARY)  ENDIF (NOT FFTW3_FIND_QUIETLY)ENDIF (FFTW3_INCLUDE_DIR AND FFTW3_LIBRARY)IF (HAVE_FFTW3)  IF (NOT FFTW3_FIND_QUIETLY)    MESSAGE (STATUS "Found components for FFTW3")    MESSAGE (STATUS "FFTW3_INCLUDE_DIR = ${FFTW3_INCLUDE_DIR}")    MESSAGE (STATUS "FFTW3_LIBRARY     = ${FFTW3_LIBRARY}")  ENDIF (NOT FFTW3_FIND_QUIETLY)ELSE (HAVE_FFTW3)  IF (FFTW3_FIND_REQUIRED)    MESSAGE (FATAL_ERROR "Could not find FFTW3!")  ENDIF (FFTW3_FIND_REQUIRED)ENDIF (HAVE_FFTW3)
+## ------------------------------------------------------------------------------## Mark as advanced ...
