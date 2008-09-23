68e126b1594d9f051e56114e605e218b1d5db2ea
maestro <maestro@12089078-0fe9-4cd0-90fa-723a746d8aa5>
2008-09-23 13:13:40 +0000

    initial checkin

---
+++ manyears/cameraBridge/CMakeFiles/cameraBridge.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/dominic/WA/ManyEars/cameraBridge/main.cpp" "/Users/dominic/WA/ManyEars/cameraBridge/CMakeFiles/cameraBridge.dir/main.cpp.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+SET(CMAKE_TARGET_DEFINITIONS
+  "SPHINX_INSTALL_DIR=\"/usr/local/lib/flowdesigner/toolbox//ManyEars/sphinx4\""
+  "QT_DLL"
+  "QT_WEBKIT_LIB"
+  "QT_GUI_LIB"
+  "QT_NETWORK_LIB"
+  "QT_CORE_LIB"
+  "QT_NO_DEBUG"
+  )
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ manyears/cameraBridge/CMakeFiles/cameraBridge.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/cameraBridge.dir/main.cpp.o"
+  "cameraBridge.pdb"
+  "cameraBridge"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/cameraBridge.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
